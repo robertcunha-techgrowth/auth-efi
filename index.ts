83725a6cfeb13736add3e01ce5a8174dd73dee03
Robert Cunha <robert.cunha@techgrowth-labs.com.br>
2024-12-06 00:49:04 -0300

    fix: load variables

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
-import { AppModule } from "./src/app.module";
 import { ConfigModule } from "./src/core/infrastructure/config.module";
-import { Handler } from "./src/core/presenter/handler";
-
 ConfigModule.forRoot({ path: ".env" });
+import { AppModule } from "./src/app.module";
+
+import { Handler } from "./src/core/presenter/handler";
 
 const handlerObject = new Handler(AppModule);
 export const handler = handlerObject.handler.bind(handlerObject);
