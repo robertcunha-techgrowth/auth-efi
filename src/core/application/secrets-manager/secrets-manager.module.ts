1ef869b75c0b83021cad42293fec54dff924eb10
Robert Cunha <robert.cunha@techgrowth-labs.com.br>
2024-12-13 01:04:11 -0300

    refactor: variables

--- src/core/application/secrets-manager/secrets-manager.module.ts
+++ src/core/application/secrets-manager/secrets-manager.module.ts
@@ -5,19 +5,15 @@ import {
 	Module,
 } from "@techgrowth-labs/dependency-injection";
 import { SecretsManagerService } from "./secrets-manager.service";
-import * as dotenv from "dotenv";
 
 export const SecretsManagerClientProvider = new FactoryProvider({
 	provide: "SecretsManagerClient",
 	useFactory: () => {
-		console.log("AWS_REGION", process.env.AWS_REGION);
-		console.log("AWS_ACCESS_KEY_ID", process.env.AWS_ACCESS_KEY_ID);
-		console.log("AWS_SECRET_ACCESS_KEY,", process.env.AWS_SECRET_ACCESS_KEY);
 		return new SecretsManagerClient({
-			region: process.env.AWS_REGION,
+			region: process.env.AWS_REGION_SSM,
 			credentials: {
-				accessKeyId: process.env.AWS_ACCESS_KEY_ID as string,
-				secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string,
+				accessKeyId: process.env.AWS_ACCESS_KEY_ID_SSM as string,
+				secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY_SSM as string,
 			},
 		});
 	},
