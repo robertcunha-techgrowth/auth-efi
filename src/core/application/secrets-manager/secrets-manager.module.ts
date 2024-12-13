2565641e7c83d301a12cd2d77c05121d37adf5c1
Robert Cunha <robert.cunha@techgrowth-labs.com.br>
2024-12-13 00:49:48 -0300

    refactor: log

--- src/core/application/secrets-manager/secrets-manager.module.ts
+++ src/core/application/secrets-manager/secrets-manager.module.ts
@@ -10,7 +10,9 @@ import * as dotenv from "dotenv";
 export const SecretsManagerClientProvider = new FactoryProvider({
 	provide: "SecretsManagerClient",
 	useFactory: () => {
-		dotenv.config();
+		console.log("AWS_REGION", process.env.AWS_REGION);
+		console.log("AWS_ACCESS_KEY_ID", process.env.AWS_ACCESS_KEY_ID);
+		console.log("AWS_SECRET_ACCESS_KEY,", process.env.AWS_SECRET_ACCESS_KEY);
 		return new SecretsManagerClient({
 			region: process.env.AWS_REGION,
 			credentials: {
